7ed8cd0c1c84425539e3b7a4ea2a269287259a28
Ricardo <Usuario@CPD03-M05>
2024-08-19 12:10:12 -0300

    DataService adicionado e mainpage configurado

---
+++ MauiTempoAgora/Service/DataServiceJsonNet8.cs
@@ -0,0 +1,11 @@
+﻿using MauiTempoAgora.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace MauiTempoAgora.Service
+{
+    public class DataServiceJsonNet8
+    {
+        
+    }
+}
